ea4c3c3b474e997e241e16a9a644488cc687cae1
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-01-18 01:58:17 +0300

    Add files via upload

---
+++ components/message/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './message.module.css'
+export const Message = ({msgProps}) => {
+   return (<p className={styles.reset}>
+      Random msg
+   </p>)
+}
