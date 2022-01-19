8678c98c3aecb2c4ea171e93e2171a86c59b2104
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-01-19 03:22:15 +0300

    Update index.js

--- components/message/index.js
+++ components/message/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styles from './message.module.css'
 export const Message = ({msgProps}) => {
-   return (<p className={styles.reset}>
-      Random msg
-   </p>)
+   return (<p className={styles.reset}>{msgProps}</p>)
 }
