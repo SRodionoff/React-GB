49460bfae5c23e00af9d151d680b65d99d5008bb
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-01-21 12:45:15 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import { Message, MessageList } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <MessageList />
+    </div>
+
+  );
+}
+
+export default App;
