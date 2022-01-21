1bafcbac36af41f841a832e72801433330ffa3f9
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-01-21 12:42:33 +0300

    Delete components directory

--- components/button/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import styles from './button.module.css'
-export const Button = (props) => {
-   return (<button onClick={props.onClick} disabled={props.disabled} className={[styles.reset]}>
-      btn
-   </button>)
-}
--- components/message/index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import styles from './message.module.css'
-export const Message = ({msgProps}) => {
-   return (<p className={styles.reset}>{msgProps}</p>)
-}
