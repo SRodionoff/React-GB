eca195ad63e70df7390a9246cfcd13b6f3d75c66
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-01-25 14:39:45 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ReactDOM from 'react-dom';
+import { Message, MessageList } from './components';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <MessageList />
+    </div >
+  );
+}
+
+export default App;
