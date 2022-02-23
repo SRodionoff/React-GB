1aaf78965a3160721bf3d0614adf8ea0268c7b43
SRodionoff <87777879+SRodionoff@users.noreply.github.com>
2022-02-23 18:32:30 +0300

    Delete src directory

--- src/components/Error.js
+++
@@ -1,8 +0,0 @@
-import { Alert, Button } from "react-bootstrap";
-
-export const Error = ({ reload }) => (
-    <Alert variant="danger">
-        Oops, something went wrong!
-        <Button onClick={reload}>Reload</Button>
-    </Alert>
-);
